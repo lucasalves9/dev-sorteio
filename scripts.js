24892b2d7379ac5be0b500d2827c909f280030cc
lucasalves9 <lucasalvessousaa@gmail.com>
2025-03-31 09:33:27 -0300

    meu dev sorteio

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+
+    if (min >= max) {
+        alert("O valor minimo tem que ser MENOR que o valor máximo")
+    } else {
+
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        document.getElementById("resultado").value = result;
+    }
+
+
+}
+
+
+
